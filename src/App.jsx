79d85a800d1b915bc5f86c5777281bfd8247635e
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-08-18 20:09:27 -0300

    feat:requisição adicionada ao card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 import "./App.css";
 import Card from "./components/Card/Card";
 import Forms from "./components/Forms/Forms";
 
 function App() {
-
-  const [req, setReq] = useState()
+  const [req, setReq] = useState();
 
   return (
     <div className="App">
-      <Forms/>
-      <Card/>
+      <Forms setReq={setReq} />
+      <Card
+        cep={req ? req.cep : ""}
+        logradouro={req ? req.logradouro : ""}
+        complemento={req ? req.complemento : ""}
+        bairro={req ? req.bairro : ""}
+        localidade={req ? req.localidade : ""}
+        uf={req ? req.uf : ""}
+      />
     </div>
   );
 }
